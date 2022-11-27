a4d3fea16051a838bdaea9257b92783abc95eb8a
Jagroop Singh <jagroopofficial2000@gmail.com>
2022-11-27 18:54:18 +0530

    HeaderComponent

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   <Header />
+
+   <div className="container-fluid p-0 mb-5">
+       <div className="owl-carousel header-carousel position-relative">
+           <div className="owl-carousel-item position-relative">
+               <img className="img-fluid" src="img/carousel-1.jpg" alt="" />
+               <div className="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center" >
+                   <div className="container">
+                       <div className="row justify-content-start">
+                           <div className="col-sm-10 col-lg-8">
+                               <h5 className="text-primary text-uppercase mb-3 animated slideInDown">Best Online Courses</h5>
+                               <h1 className="display-3 text-white animated slideInDown">The Best Online Learning Platform</h1>
+                               <p className="fs-5 text-white mb-4 pb-2">Vero elitr justo clita lorem. Ipsum dolor at sed stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea sanctus eirmod elitr.</p>
+                               <a href="" className="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Read More</a>
+                               <a href="" className="btn btn-light py-md-3 px-md-5 animated slideInRight">Join Now</a>
+                           </div>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+   </div>
+   </>
+
   );
 }
 
